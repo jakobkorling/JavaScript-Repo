7ea19b0b527edb1f35282ca4169711a740c3404a
jakobkorling <jakobkorling@gmail.com>
2026-03-18 11:29:22 +0100

    Assignement 2 final commit

--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
+// Assignement 1 // 
+
 console.log("Hello, World!");
 
 let a;
@@ -21,3 +23,20 @@ console.log(X + Y);
 console.log(X - Y);
 console.log(X * Y);
 console.log(X / Y);
+
+// Assignement 2 // 
+
+// Prompt message 
+let input = prompt("Enter price tag $49.99");
+
+// Replaces dollar sign to nothing
+let noDollarSign = input.replace("$", "");
+
+//Declares priceTag to be a number and not text
+let priceTag = Number(noDollarSign);
+
+// Declares discount to be value of priceTag * 0.9 (10%)
+let discount = priceTag * 0.9;
+
+// Prints "Your discounted price is: $(value of entered numbers * 0.9 with only 2 decimals)"
+console.log("Your discounted price is: $" + discount.toFixed(2));
