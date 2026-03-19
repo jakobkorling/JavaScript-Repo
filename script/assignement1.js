51b94a68a4998f0f1e8366e9c58169f9a4a40013
jakobkorling <jakobkorling@gmail.com>
2026-03-19 10:30:54 +0100

    changed names on js-files

---
+++ script/assignement1.js
@@ -0,0 +1,26 @@
+// Assignement 1 // 
+
+console.log("Hello, World!");
+
+let a;
+let b;
+let c;
+
+a = "Jakob";
+b = 30;
+c = "green"; 
+
+console.log(`My name is ${a}. I am ${b} years old and my favorite color is ${c}.`)
+
+
+let X;
+let Y;
+
+Y = 10;
+X = 20;
+
+console.log(X + Y);
+console.log(X - Y);
+console.log(X * Y);
+console.log(X / Y);
+
