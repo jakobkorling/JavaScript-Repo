9a3a59e553ca845d9667e3647911541e07a78827
jakobkorling <jakobkorling@gmail.com>
2026-04-13 14:13:54 +0200

    Added ATM function in assignement 5

--- script/assignement5.js
+++ script/assignement5.js
@@ -3,7 +3,7 @@ const account = {
     balance: 1337,
 
     getBalance: function () {
-        console.log(`Balance: ${this.balance} SEK`);
+        alert(`Balance: ${this.balance} SEK`);
     },
 
     deposit: function (amount) {
@@ -12,23 +12,54 @@ const account = {
             return;
         }
         this.balance += amount;
-        console.log(`Deposited: ${amount} SEK`);
+        alert(`Deposited: ${amount} SEK`);
     },
 
     withdraw: function (amount) {
         if (amount > this.balance) {
-            this.accountError("You don't have enough funds on your account.");
+            this.accountError("You don't have enough funds on your account!");
             return;
         }
         this.balance -= amount;
-        console.log(`Withdrew: ${amount} SEK`);
+        alert(`Withdrew: ${amount} SEK`);
     },
 
     getAccountName: function () {
-        console.log(`Name of bank account: ${this.accountName}`);
+        alert(`Name of bank account: ${this.accountName}`);
     },
 
     accountError: function (message) {
-        console.error(`Èrror: ${message}`);
+        alert(`Error: ${message}`);
     }
 };
+
+function atm() {
+    const message = parseFloat(
+        prompt(
+            "Select a choice \n1.) Get balance \n2.) Make a deposit \n3.) Make a withdrawal \n4.) Get account name"
+        )
+    );
+
+    switch (message) {
+        case 1:
+            account.getBalance();
+            atm();
+            break;
+        case 2:
+            const depositAmount = parseFloat(prompt("Enter amount:"));
+            account.deposit(depositAmount);
+            atm();
+            break;
+        case 3:
+            const withdrawAmount = parseFloat(prompt("Enter amount:"));
+            account.withdraw(withdrawAmount);
+            atm();
+            break;
+        case 4:
+            account.getAccountName();
+            atm();
+            break;
+    }
+}
+
+atm();
